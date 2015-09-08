f42c890a27ed8e08c0e5936e7136b1af0c42ef05
Franco Testori <franco.testori@ing.austral.edu.ar>
2015-09-07 21:36:35 -0300

    Thursdays sprint session

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -13,22 +13,24 @@ object Application extends Controller {
     val db = Database.forURL("jdbc:h2:file:~/projects/shelob/db/db","sa","")
 
     try{
-//
-//    val urls : List[String] = CSVProcessor.process("C:\\users-argentina.csv")
-//
-//      LinkedInWizard.run(urls)
-//
-//      CSVProcessor.writeLO(LinkedInWizard.getOwnerTable, "C:\\Users\\franco\\shelobItems\\owners.csv")
-//      CSVProcessor.writeBI(LinkedInWizard.getInstitutionTable, "C:\\Users\\franco\\shelobItems\\institution.csv")
-//      CSVProcessor.writeBB(LinkedInWizard.getBBTable, "C:\\Users\\franco\\shelobItems\\bbackground.csv")
-//      CSVProcessor.writeAI(LinkedInWizard.getAcademyTable, "C:\\Users\\franco\\shelobItems\\academy.csv")
-//      CSVProcessor.writeAB(LinkedInWizard.getABTable, "C:\\Users\\franco\\shelobItems\\abackground.csv")
 
-      ZipGenerator.zip("C:\\Users\\franco\\shelobItems\\shelob.zip", Iterable("C:\\Users\\franco\\shelobItems\\owners.csv",
-        "C:\\Users\\franco\\shelobItems\\institution.csv",
-        "C:\\Users\\franco\\shelobItems\\bbackground.csv",
-        "C:\\Users\\franco\\shelobItems\\academy.csv",
-        "C:\\Users\\franco\\shelobItems\\abackground.csv"
+    val urls : List[String] = CSVProcessor.process("C:\\users-argentina.csv")
+
+      LinkedInWizard.run(urls)
+
+      CSVProcessor.writeLO(LinkedInWizard.getOwnerTable, "C:\\Users\\franco\\shelobItems\\personas.csv")
+      CSVProcessor.writeBI(LinkedInWizard.getInstitutionTable, "C:\\Users\\franco\\shelobItems\\negocio.csv")
+      CSVProcessor.writeBB(LinkedInWizard.getBBTable, "C:\\Users\\franco\\shelobItems\\experiencia.csv")
+      CSVProcessor.writeAI(LinkedInWizard.getAcademyTable, "C:\\Users\\franco\\shelobItems\\academia.csv")
+      CSVProcessor.writeAB(LinkedInWizard.getABTable, "C:\\Users\\franco\\shelobItems\\historial-academico.csv")
+
+      ZipGenerator.zip("~\\shelob.zip",
+
+        Iterable("C:\\Users\\franco\\shelobItems\\personas.csv",
+        "C:\\Users\\franco\\shelobItems\\negocio.csv",
+        "C:\\Users\\franco\\shelobItems\\experiencia.csv",
+        "C:\\Users\\franco\\shelobItems\\academia.csv",
+        "C:\\Users\\franco\\shelobItems\\historial-academico.csv"
       ))
 
     }finally db.close()
